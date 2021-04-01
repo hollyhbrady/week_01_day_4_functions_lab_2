29dadf4ab9379df05be036a2187d86285b4ee309
hollyhbrady <hollyhbrady@gmail.com>
2021-04-01 19:50:48 +0100

    Mark v Holly's work

--- task_list.py
+++ task_list.py
@@ -8,12 +8,11 @@ tasks = [
     { "description": "Walk Dog", "completed": True, "time_taken": 60 },
 ]
 
-
-
 # MVP
 
 # As a user, to manage my task list I would like a program that allows me to:
-# Print a list of uncompleted tasks
+
+# 1 Print a list of uncompleted tasks
 def check_to_do_list(list, completed):
     # pdb.set_trace()
     found_tasks = []
@@ -25,7 +24,7 @@ def check_to_do_list(list, completed):
 
 print(check_to_do_list(tasks, False))
 
-# Print a list of completed tasks
+# 2 Print a list of completed tasks
 
 def check_to_do_list(list, completed):
     # pdb.set_trace()
@@ -37,70 +36,70 @@ def check_to_do_list(list, completed):
     return found_tasks
 
 print(check_to_do_list(tasks, True))
-# Print a list of all task descriptions
+# 3 Print a list of all task descriptions
 
-def print_task_descriptions(list):
-    descriptions =[]
-    for task in tasks:
-        descriptions.append(task["description"])
-    return descriptions
+# def print_task_descriptions(list):
+#     descriptions =[]
+#     for task in tasks:
+#         descriptions.append(task["description"])
+#     return descriptions
 
-print(print_task_descriptions(tasks))
+# print(print_task_descriptions(tasks))
 
-# Print a list of tasks where time_taken is at least a given time
+# 4 Print a list of tasks where time_taken is at least a given time
 
-def print_task_with_x_time(list, time):
-    found_task = []
+# def print_task_with_x_time(list, time):
+#     found_task = []
 
-    for task in tasks:
-        if task["time_taken"] <= time:
-            found_task.append(task["description"])
-    return found_task
+#     for task in tasks:
+#         if task["time_taken"] <= time:
+#             found_task.append(task["description"])
+#     return found_task
 
-print(print_task_with_x_time(tasks, 15))
+# print(print_task_with_x_time(tasks, 15))
 
-# Print any task with a given description 
+# 5 Print any task with a given description 
 
-def print_task_with_x_description(list, description):
-    found_task = []
+# def print_task_with_x_description(list, description):
+#     found_task = []
 
-    for task in tasks:
-        if task["description"] == description:
-            found_task.append(task)
-    return found_task
+#     for task in tasks:
+#         if task["description"] == description:
+#             found_task.append(task)
+#     return found_task
 
-print(print_task_with_x_description(tasks, "Feed Cat"))
+# print(print_task_with_x_description(tasks, "Feed Cat"))
 
-# Extension
+# *Extension*
 
-# Given a description update that task to mark it as complete.
+# 6 Given a description update that task to mark it as complete.
 
-def set_task_as_complete(list, updated_task):
+# def set_task_as_complete(list, updated_task):
 
-    for task in tasks:
-        if task["description"] == updated_task:
-            task["completed"] = True
-    return f'{updated_task} has been completed'
+#     for task in tasks:
+#         if task["description"] == updated_task:
+#             task["completed"] = True
+#     return f'{updated_task} has been completed'
 
-print(set_task_as_complete(tasks, "Feed Cat")) 
+# print(set_task_as_complete(tasks, "Feed Cat")) 
 
-# Add a task to the list
+# 7 Add a task to the list
 
 #append tasks list with new dictionary
 #create new task dictionary
 #add new task dictionary to list of tasks 
 
-def add_task_to_list(list, new_task):
-    new_task = {"description": new_task, "completed": False, "time_taken": "One does not simply 'complete' Warzone"}
+# def add_task_to_list(list, new_task):
+#     new_task = {"description": new_task, "completed": False, "time_taken": "One does not simply 'complete' Warzone"}
     
-    tasks.append(new_task)
-    return tasks 
+#     tasks.append(new_task)
+#     return tasks 
 
-add_task_to_list(tasks, "Warzone")
+# add_task_to_list(tasks, "Warzone")
 
-print(tasks[5])
+# print(tasks[5])
 
-# Further Extensions
+# *Further Extensions*
 
-# Use a while loop to display the following menu and allow the use to enter an option.
+# 8 Use a while loop to display the following menu and allow the use to enter an option.
 
